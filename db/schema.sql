1f12971ede5758fcd164a88ecb4d1cdaf7544715
King David <103971670+kyle-david1@users.noreply.github.com>
2022-08-01 19:56:12 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,13 @@ CREATE DATABASE team_db;
 
 USE team_db;
 
--- table for department
+
 CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL
 );
 
--- table for role
+
 CREATE TABLE roles (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE roles (
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE 
 );
 
--- table for employee
+
 CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
@@ -27,9 +27,10 @@ CREATE TABLE employee (
   role_id INT NOT NULL,
   manager_id INT NULL,
   INDEX role_ind (role_id),
+	INDEX manager_ind (manager_id),
   CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES
-  roles(id)
-  FOREIGN KEY(manager_id) REFERENCES employees(id)
-  manager_id INT NULL
+  roles(id),
+  FOREIGN KEY(manager_id) REFERENCES employee(id)
+  
   ON DELETE CASCADE
 );
